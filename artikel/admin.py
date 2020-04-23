ee46e49188c0fd024fdfd6297a772b84886b3312
Ray <raytommy1234@gmail.com>
2020-04-23 21:01:47 +0800

    membuat website blog

---
+++ artikel/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Artikel
+
+
+class ArtikelAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        'slug',
+        'updated',
+        'published',
+    ]
+
+
+admin.site.register(Artikel, ArtikelAdmin)
